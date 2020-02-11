e32231cc4aeecad9474df51ac67d77aeb8d80468
Douglas <douglas.jesuscosta@outlook.com>
2020-02-11 19:05:11 -0300

    Creating folders to group projects

---
+++ Back-end/ServicePedidos/src/main/java/com/ecommerceproject/service/pedidos/ServicePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceproject.service.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePedidosApplication.class, args);
+	}
+
+}
---
+++ Front-end/ecommerceproject/src/app/pages/pedidosmodule/pedidos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PedidoComponent } from './pedido/pedido.component';
+
+
+
+@NgModule({
+  declarations: [PedidoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PedidosModule { }
